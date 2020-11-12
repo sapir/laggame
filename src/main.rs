1a348eb08398a110d5d3a55a8c00d4678451b795
sapir <yasapir@gmail.com>
2020-11-12 02:48:37 +0200

    Modify to use mouse

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,8 @@
 use bevy::prelude::*;
-use bevy_rapier2d::{
-    na::Vector2,
-    physics::{RapierConfiguration, RapierPhysicsPlugin, RigidBodyHandleComponent},
-    rapier::{
-        dynamics::{RigidBodyBuilder, RigidBodySet},
-        geometry::ColliderBuilder,
-    },
-};
 
 fn main() {
     App::build()
         .add_default_plugins()
-        .add_plugin(RapierPhysicsPlugin)
-        .add_resource(RapierConfiguration {
-            gravity: Vector2::zeros(),
-            ..Default::default()
-        })
         .add_resource(WindowDescriptor {
             title: "Game!".to_string(),
             width: 2000,
@@ -31,57 +18,42 @@ fn main() {
         .run();
 }
 
-pub fn setup_camera(mut commands: Commands) {
+fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2dComponents::default());
 }
-pub struct Player;
-pub struct PlayerSkin(Handle<ColorMaterial>);
-pub struct PlayerMaxVelocity(f32);
+struct Player;
+struct PlayerSkin(Handle<ColorMaterial>);
 
-pub fn setup_resources(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+fn setup_resources(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     commands.insert_resource(PlayerSkin(materials.add(Color::rgb(0.3, 0.7, 0.7).into())));
 }
 
-pub fn setup_player(mut commands: Commands, player_skin: Res<PlayerSkin>) {
+fn setup_player(mut commands: Commands, player_skin: Res<PlayerSkin>) {
     commands
         .spawn(SpriteComponents {
             material: player_skin.0,
             sprite: Sprite::new(Vec2::new(100.0, 100.0)),
             ..Default::default()
         })
-        .with(RigidBodyBuilder::new_dynamic())
-        .with(ColliderBuilder::cuboid(100.0, 100.0))
-        .with(Player)
-        .with(PlayerMaxVelocity(1000.));
+        .with(Player);
 }
 
-pub fn movement(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut bodies: ResMut<RigidBodySet>,
-    query: Query<&RigidBodyHandleComponent>,
-    mut players_pos: Query<(&Player, Entity, &PlayerMaxVelocity)>,
-) {
-    for (_player, ent, max_v) in &mut players_pos.iter() {
-        if let Ok(body_handle) = query.get::<RigidBodyHandleComponent>(ent) {
-            let mut body = bodies.get_mut(body_handle.handle()).unwrap();
-            body.wake_up(true);
-            let mut direction = Vector2::zeros();
-            if keyboard_input.pressed(KeyCode::Left) {
-                direction = Vector2::x() * -1.;
-            }
-            if keyboard_input.pressed(KeyCode::Right) {
-                direction = Vector2::x();
-            }
-            if keyboard_input.pressed(KeyCode::Down) {
-                direction = Vector2::y() * -1.;
-            }
-            if keyboard_input.pressed(KeyCode::Up) {
-                direction = Vector2::y();
-            }
+#[derive(Default)]
+struct CursorEventState(EventReader<CursorMoved>);
 
-            body.linvel = direction * max_v.0;
-            // body.apply_force(direction * max_v.0);
-            // info!("{:?}", body.linvel,);
+fn movement(
+    mut state: Local<CursorEventState>,
+    cursor_events: Res<Events<CursorMoved>>,
+    windows: Res<Windows>,
+    mut players_pos: Query<(&Player, &mut Transform)>,
+) {
+    let window = windows.get_primary().unwrap();
+
+    if let Some(moved) = state.0.latest(&cursor_events) {
+        for (_player, mut transform) in &mut players_pos.iter() {
+            let pos =
+                moved.position - Vec2::new(window.width as f32 / 2.0, window.height as f32 / 2.0);
+            transform.set_translation(pos.extend(0.0));
         }
     }
 }
